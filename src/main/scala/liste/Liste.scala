7ca6de6691ada0362cc5a46320295e1e3a7e5730
qw0te <baert.quentin@gmail.com>
2015-02-27 18:54:06 +0100

    Ajoute le fichier Liste.scala
    Ce fichier contient les classes permettant l'implémentation des listes
    d'entiers

---
+++ src/main/scala/liste/Liste.scala
@@ -0,0 +1,105 @@
+/**
+ * Classe qui permet de gérer les listes d'entiers
+ *
+ * @author Quentin Baert
+ */
+
+trait Liste {
+  
+  /**
+   * Donne la tête de la liste
+   *
+   * @return tête de la liste
+   */
+  def head: Int
+
+  /**
+   * Donne la queue de la liste
+   *
+   * @return queue de la liste
+   */
+  def tail: Liste
+
+  /**
+   * Determine si la liste est vide ou non
+   *
+   * @return true si la liste est vide, false sinon
+   */
+  def isEmpty: Boolean
+
+  /**
+   * Ajoute un élément en queue de liste
+   *
+   * @param elem
+   *          élément à ajouter queue de liste
+   * @return nouvelle liste avec l'élément ajouté en queue
+   */
+  def add(elem: Int): Liste
+  
+}
+
+object ListeVide extends Liste {
+
+  /**
+   * @see liste.Liste#head
+   */
+  override def head: Int = throw new Error("ListeVide.head")
+
+  /**
+   * @see liste.Liste#tail
+   */
+  override def tail: Liste = throw new Error("ListeVide.tail")
+
+  /**
+   * @see liste.Liste#isEmpty
+   */
+  override def isEmpty: Boolean = true
+
+  /**
+   * @see liste.Liste#add
+   */
+  override def add(elem: Int): ListeEntiers =
+    new ListeEntiers(elem)
+
+}
+
+case class ListeEntiers(private val headConst: Int, private val tailConst: Liste) extends Liste {
+
+  /**
+   * Second constructeur qui permet de créer une liste uniquement à partir
+   * d'un entier
+   *
+   * @param head
+   *          entier à partir duquel créer la liste
+   */
+  def this(head: Int) =
+    this(head, ListeVide)
+
+  /**
+   * @see liste.Liste#head
+   */
+  override def head: Int = this.headConst
+
+  /**
+   * @see liste.Liste#tail
+   */
+  override def tail: Liste = this.tailConst
+
+  /**
+   * @see liste.Liste#isEmpty
+   */
+  override def isEmpty: Boolean = false
+
+  /**
+   * @see liste.Liste#add
+   */
+  override def add(elem: Int): Liste = {
+    val newElem = new ListeEntiers(elem)
+    if (this.tail.isEmpty)
+      new ListeEntiers(this.head, new ListeEntiers(elem))
+    else
+      new ListeEntiers(this.head, this.tail.add(elem))
+  }
+
+}
+
