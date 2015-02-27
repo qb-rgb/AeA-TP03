d5dc6b0f63c89e1f1492bcf57231a80dd703f06f
qw0te <baert.quentin@gmail.com>
2015-02-27 21:44:19 +0100

    Ajoute de la ScalaDoc

--- src/main/scala/graphe/Graphe.scala
+++ src/main/scala/graphe/Graphe.scala
@@ -3,6 +3,14 @@ class Graphe(val mots: Array[String], val listeSucc: Array[Liste]) {
   // Nombre de mots
   private val nb: Int = mots.length
 
+  /**
+   * Ajoute une arête au graphe
+   *
+   * @param mot1
+   *          première extrémité de l'arête ajoutée
+   * @param mot2
+   *          seconde extrémité de l'arête ajoutée
+   */
   def ajouterArete(mot1: Int, mot2: Int): Unit = {
     val succMot1 = this.listeSucc(mot1).add(mot2)
     val succMot2 = this.listeSucc(mot2).add(mot1)
